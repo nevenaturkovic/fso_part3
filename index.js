c4846b0ca1cc23d112e0514cf928905fe6ccc231
Nevena Turkovic <nevenatu@gmail.com>
2022-01-28 14:55:17 +0100

    Phonebook backend step5

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const app = express()
 
+app.use(express.json())
+
 let persons = [
   {
     id: 1,
@@ -53,9 +55,32 @@ app.get("/api/persons/:id", (request, response) => {
 
 app.delete("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id)
-  persons = persons.filter((note) => note.id !== id)
+  persons = persons.filter((person) => person.id !== id)
 
   response.status(204).end()
+})
+
+const generateId = () => {
+  return Math.floor(Math.random() * 1000000)
+}
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body
+  if (!body.name) {
+    return response.status(400).json({
+      error: "content missing",
+    })
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
 })
 
 const PORT = 3001
