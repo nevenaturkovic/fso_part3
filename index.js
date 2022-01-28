40dad92ee8cdf8b9ed703be8f6cf9f640b14c364
Nevena Turkovic <nevenatu@gmail.com>
2022-01-28 10:55:08 +0100

    Phonebook beckend step2

--- index.js
+++ index.js
@@ -28,6 +28,14 @@ app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>")
 })
 
+app.get("/info", (request, response) => {
+  const numberOfPersons = persons.length
+  response.send(`
+  <p>Phonebook has info for ${numberOfPersons} people</p>
+  <p>${new Date()}</p>
+  `)
+})
+
 app.get("/api/persons", (request, response) => {
   response.json(persons)
 })
