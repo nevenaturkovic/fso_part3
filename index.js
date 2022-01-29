262b00d653293d068fc56a8331c52fa01eb3a9f7
Nevena Turkovic <nevenatu@gmail.com>
2022-01-29 15:00:30 +0100

    Phonebook backend step7

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require("express")
+const morgan = require("morgan")
+
 const app = express()
 
 app.use(express.json())
+app.use(morgan('tiny'))
+
+// const requestLogger = (request, response, next) => {
+//   console.log('Method:', request.method)
+//   console.log('Path:  ', request.path)
+//   console.log('Body:  ', request.body)
+//   console.log('---')
+//   next()
+// }
+// app.use(requestLogger)
 
 let persons = [
   {
