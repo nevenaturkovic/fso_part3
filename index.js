b725e2263d833992614d45b37b5de0bd7d8cb6e7
Nevena Turkovic <nevenatu@gmail.com>
2022-02-04 12:47:58 +0100

    Added build directory

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 
 morgan.token("jsoncontent", (request, response) => JSON.stringify(request.body))
 app.use(
