c05a9537ba84e7e5b09f2c26fbb918fa90b3a5a3
Nevena Turkovic <nevenatu@gmail.com>
2022-02-07 18:23:10 +0100

    Removed the comments from index.js

--- index.js
+++ index.js
@@ -16,41 +16,6 @@ app.use(
   )
 )
 
-// let persons = [
-//   {
-//     id: 1,
-//     name: "Arto Hellas",
-//     number: "040-123456",
-//   },
-//   {
-//     id: 2,
-//     name: "Ada Lovelace",
-//     number: "39-44-5323523",
-//   },
-//   {
-//     id: 3,
-//     name: "Dan Abramov",
-//     number: "12-43-234345",
-//   },
-//   {
-//     id: 4,
-//     name: "Mary Poppendieck",
-//     number: "39-23-6423122",
-//   },
-// ]
-
-// app.get("/", (request, response) => {
-//   response.send("<h1>Hello World!</h1>")
-// })
-
-// app.get("/info", (request, response) => {
-//   const numberOfPersons = persons.length
-//   response.send(`
-//   <p>Phonebook has info for ${numberOfPersons} people</p>
-//   <p>${new Date()}</p>
-//   `)
-// })
-
 app.get("/api/persons", (request, response) => {
   Person.find({}).then((persons) => {
     response.json(persons)
