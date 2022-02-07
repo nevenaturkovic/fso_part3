9458a79cb8cecfb3a36efe241b0d39a54754454f
Nevena Turkovic <nevenatu@gmail.com>
2022-02-07 18:18:36 +0100

    Finished 3.14 phonebook database, step2

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ app.get("/api/persons", (request, response) => {
 
 app.post("/api/persons", (request, response) => {
   const body = request.body
-  
+
   if (body === undefined) {
     return response.status(400).json({ error: "content missing" })
   }
@@ -92,55 +92,6 @@ const unknownEndpoint = (request, response) => {
 }
 
 app.use(unknownEndpoint)
-
-// app.get("/api/persons/:id", (request, response) => {
-//   const id = Number(request.params.id)
-//   const person = persons.find((person) => person.id === id)
-
-//   if (person) {
-//     response.json(person)
-//   } else {
-//     response.status(404).end()
-//   }
-// })
-
-// app.delete("/api/persons/:id", (request, response) => {
-//   const id = Number(request.params.id)
-//   persons = persons.filter((person) => person.id !== id)
-
-//   response.status(204).end()
-// })
-
-// const generateId = () => {
-//   return Math.floor(Math.random() * 1000000)
-// }
-
-// app.post("/api/persons", (request, response) => {
-//   const body = request.body
-//   if (!body.name || !body.number) {
-//     return response.status(400).json({
-//       error: "content missing",
-//     })
-//   }
-
-//   const name = request.body.name
-//   const existingName = persons.find((person) => person.name === name)
-//   if (existingName) {
-//     return response.status(422).json({
-//       error: "name must be unique",
-//     })
-//   }
-
-//   const person = {
-//     id: generateId(),
-//     name: body.name,
-//     number: body.number,
-//   }
-
-//   persons = persons.concat(person)
-
-//   response.json(person)
-// })
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
