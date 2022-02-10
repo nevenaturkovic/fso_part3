fdb1d4647744aedf8e7aa2d60b043274dccbf647
Nevena Turkovic <nevenatu@gmail.com>
2022-02-10 20:11:07 +0100

    Finished 3.18: Phonebook database step6

--- index.js
+++ index.js
@@ -9,22 +9,31 @@ app.use(express.static("build"))
 app.use(cors())
 app.use(express.json())
 
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
-
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } 
-
-  next(error)
-}
-
 morgan.token("jsoncontent", (request, response) => JSON.stringify(request.body))
 app.use(
   morgan(
     ":method :url :status :res[content-length] - :response-time ms :jsoncontent"
   )
 )
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" })
+  }
+
+  next(error)
+}
+
+app.get("/info", (request, response) => {
+  const numberOfPersons = Person.estimatedDocumentCount().then((count) => {
+    response.send(`
+    <p>Phonebook has info for ${count} people</p>
+    <p>${new Date()}</p>
+    `)
+  })
+})
 
 app.get("/api/persons", (request, response) => {
   Person.find({}).then((persons) => {
@@ -65,6 +74,21 @@ app.delete("/api/persons/:id", (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
     .then((result) => {
       response.status(204).end()
+    })
+    .catch((error) => next(error))
+})
+
+app.put("/api/persons/:id", (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      response.json(updatedPerson)
     })
     .catch((error) => next(error))
 })
